c471e9d0d73f631a4f760e25abc19286632c0000
Nisarg Chaudhari <54911392+nisargsc@users.noreply.github.com>
2022-08-20 16:40:48 +0530

    Add pragma

---
+++ pragma.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+// This is a comment
+// All solidity source files are recommended to start with License Indentifier
+
+// solidity compiler version ==0.8.7
+pragma solidity 0.8.7;
+
+// solidity compiler version >=0.8.5 in the series 0.8.z
+pragma solidity ^0.8.5;
+
+// solidity compiler version greater than 0.7.2 but strictly less than 0.8.3
+pragma solidity >=0.7.2 <0.8.3; 
